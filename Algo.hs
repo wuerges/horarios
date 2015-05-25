19a93984eb0a39575e7cb95311f7fb4c9fa2b079
Emilio Wuerges <wuerges@gmail.com>
2015-05-25 18:23:14 -0300

    Working !

--- Algo.hs
+++ Algo.hs
@@ -8,42 +8,42 @@ import Debug.Trace
 
 import qualified Data.Set as S
 
-solve :: Carga -> Quadro
-solve c = traceShow (cargaEdges c) $ Quadro []
-
 
 uniq :: Ord a => [a] -> [a]
 uniq = S.toList . S.fromList
 
-type NL1 = (Integer, [Prof], Fase, Hor)
+type NL1 = (Fase, [Prof])
 type NL2 = (Fase, Hor)
 
-genHorFase :: Carga -> [NL2]
-genHorFase c =  do
+genHorFase :: Carga -> [LNode NL2]
+genHorFase c =  zip [1..] (do
     a <- uniq (map fst $ c ^. fases)
     b <- uniq (c ^. hor)
-    return (a, b)
+    return (a, b))
 
 
-genProfHorFase :: Carga -> [NL1]
-genProfHorFase c = do
-    (n, (f, p)) <- zip [1..] $ c ^. fases
-    h <- c ^. hor
-    return (n, p, f, h)
+genProfFase :: Carga -> [LNode NL1]
+genProfFase c = zip [1..] (c ^. fases)
 
 
-cargaEdge :: NL1 -> NL2 -> Maybe (NL, NL)
-cargaEdge a@(_, _, f1, h1) b@(f2, h2) 
-    | (f1 == f2) && (h1 ^. codhor == h2 ^. codhor) = Just (P1 a, P2 b)
+cargaEdge :: LNode NL1 -> LNode NL2 -> Maybe Edge
+cargaEdge a@(n1, (f1, _)) b@(n2, (f2, h2))
+    | (f1 == f2) = Just (n1, n2)
 cargaEdge _ _ = Nothing
 
-cargaEdges :: Carga -> [(NL, NL)]
+
+partEdges :: [LNode NL1] -> [LNode NL2] -> [Edge]
+partEdges p1 p2 = catMaybes (do
+    a <- p1
+    b <- p2
+    return $ cargaEdge a b)
+
+cargaEdges :: Carga -> [Edge]
 cargaEdges c = catMaybes (do
-                    a <- genProfHorFase c
+                    a <- genProfFase c
                     b <- genHorFase c
                     return $ cargaEdge a b)
                                     
-
 data NL = P1 NL1
         | P2 NL2
         deriving Show
@@ -51,6 +51,28 @@ data NL = P1 NL1
 
 type G = Gr NL ()
 
-cargaGraph :: Carga -> G
-cargaGraph = undefined
+
+type Matching = (S.Set Node, S.Set Node, S.Set Edge)
+
+directMatch :: Matching -> Edge -> Matching
+directMatch m@(pa, pb, es) (n1, n2) = case (S.member n1 pa, S.member n2 pb) of
+    (False, False) -> (S.insert n1 pa, S.insert n2 pb, S.insert (n1, n2) es)
+    _ -> m
+
+
+simpleMatching :: [Edge] -> Matching
+simpleMatching l = foldl directMatch (S.empty, S.empty, S.empty) l
+
+
+showMatching (_, _, es) = show $ S.toList es
+
+
+
+solve :: Carga -> Quadro
+solve c = traceShow (s) $ Quadro []
+    where ce = cargaEdges c
+          sm = simpleMatching ce
+          s = showMatching sm
+
+          
 
