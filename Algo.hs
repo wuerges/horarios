2f6ba62ac35740759c4cd4783dbd7d50e9e4bcd4
Emilio Wuerges <wuerges@gmail.com>
2015-06-01 00:46:21 -0300

    changed to work with graph coloring

--- Algo.hs
+++ Algo.hs
@@ -4,7 +4,9 @@ import Data.Graph.Inductive
 import Model
 import Control.Lens
 import Data.Maybe
+import Data.Either
 import Debug.Trace
+import qualified Data.Map as H
 
 import qualified Data.Set as S
 
@@ -12,6 +14,7 @@ import qualified Data.Set as S
 uniq :: Ord a => [a] -> [a]
 uniq = S.toList . S.fromList
 
+{-
 type NL1 = (Hor, Prof)
 type NL2 = (Fase, Hor, [Prof])
 
@@ -27,7 +30,6 @@ genPart2 c =  zip [1..] (do
     a <- uniq (map fst $ c ^. fases)
     b <- uniq (c ^. hor)
     return (a, b))
-
 
 cargaEdge :: LNode NL1 -> LNode NL2 -> Maybe Edge
 cargaEdge a@(n1, (f1, _)) b@(n2, (f2, h2))
@@ -51,10 +53,10 @@ data NL = P1 NL1
         | P2 NL2
         deriving Show
 
+-}
+type G = Gr Hor ()
 
-type G = Gr NL ()
-
-
+{-
 type Matching = (S.Set Node, S.Set Node, S.Set Edge)
 
 directMatch :: Matching -> Edge -> Matching
@@ -68,17 +70,36 @@ simpleMatching l = foldl directMatch (S.empty, S.empty, S.empty) l
 
 
 showMatching (_, _, es) = show $ S.toList es
+-}
 
 
+type PM = H.Map Int Prof
+type TODO = [(Prof, [Int])]
+
+color1n :: PM -> G -> Prof -> Int -> Maybe PM
+color1n pm g c n = 
+    if any (c==) (catMaybes $ map ((flip H.lookup) pm) $ neighbors g n) then Nothing
+    else Just $ H.insert n c pm
+
+color1ns :: PM -> G -> Prof -> [Int] -> Maybe PM
+color1ns pm g c = 
+    listToMaybe . catMaybes . map (color1n pm g c)
+
+color :: PM -> G -> TODO -> Either (PM, Prof) PM
+color pm g [] = Right pm
+color pm g ((c, ns):cs) = case color1ns pm g c ns of 
+    Just pm' -> color pm' g cs
+    Nothing -> Left (pm, c)
+    
 
 solve :: Carga -> Quadro
-solve c = trace pp $ Quadro []
-    where ce = cargaEdges c
-          sm = simpleMatching ce
-          s = showMatching sm
-          as = [(n, p) | (n, (f, p)) <- genProfFase c]
-          bs = [(n, h, f) | (n, (h, f)) <- (genHorFase c)]
-          pp = show s ++ "\n" ++ show as ++ "\n" ++ show bs
+solve c = trace "" $ Quadro []
+    --where ce = cargaEdges c
+     --     sm = simpleMatching ce
+      --    s = showMatching sm
+          --as = [(n, p) | (n, (f, p)) <- genProfFase c]
+          --bs = [(n, h, f) | (n, (h, f)) <- (genHorFase c)]
+          --pp = show s ++ "\n" ++ show as ++ "\n" ++ show bs
 
           
 
