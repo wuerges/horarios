4dab549dd1f1327079367a519416dfd1fbc375f5
Fernando Bevilacqua <dovyski@gmail.com>
2015-07-23 11:09:11 -0300

    First steps to allow the addition of new professors.

--- ui/js/app.js
+++ ui/js/app.js
@@ -77,6 +77,8 @@ var HORARIO = new function() {
             // Colect information related to courses
             mData.courses[aEntry[2]._nome] = aEntry[2]._nome;
         }
+
+        console.debug(mData);
     }
 
     var loadData = function(theCallback) {
@@ -107,14 +109,14 @@ var HORARIO = new function() {
                 '</div>' +
                 '<div class="row schedule-caption-row">' +
                     '<div id="caption-'+ aGroup +'" class="col-lg-10">' +
-                        renderGroupCaption(mData.professors[aGroup]) +
+                        renderGroupCaption(mData.professors[aGroup], aGroup) +
                     '</div>' +
                 '</div>'
             );
         }
     }
 
-    var renderGroupCaption = function(theProfessors) {
+    var renderGroupCaption = function(theProfessors, theGroup) {
         var aName,
             aId,
             aCourse,
@@ -125,6 +127,8 @@ var HORARIO = new function() {
                 aRet.push('<li>' + aCourse + ' (' + aName + ') </li>');
             }
         }
+
+        aRet.push('<li><a href="javascript:void(0)" data-group="' + theGroup + '" class="add-professor"><i class="fa fa-user-plus"></i> Adicionar</a></li>');
 
         return aRet.join('');
     }
@@ -202,6 +206,38 @@ var HORARIO = new function() {
         }
     };
 
+    var generateNewProfessorForm = function(theGroup) {
+        var aRet =
+            '<form class="form-inline" id="formProfessor' + theGroup + '">' +
+              '<div class="form-group">' +
+                '<input type="text" class="form-control" name="course" placeholder="Componente currucular">' +
+              '</div>' +
+              '<div class="form-group">' +
+                '<input type="text" class="form-control" name="professor" placeholder="Professor">' +
+              '</div>' +
+              '<button type="submit" class="btn btn-default"><i class="fa fa-plus"></i> Adicionar</button>' +
+            '</form>';
+
+        return aRet;
+    }
+
+    var handleNewProfessorClick = function() {
+        var aElement    = $(this),
+            aGroup      = aElement.data('group'),
+            aContainer  = aElement.parent(),
+            aForm;
+
+        aContainer.html(generateNewProfessorForm(aGroup));
+
+        $('#formProfessor' + aGroup).submit(function() {
+            // TODO: implement the remaining parts
+            aForm = document.getElementById('formProfessor' + aGroup);
+            aContainer.html(aForm.elements.course.value + ' (' + aForm.elements.professor.value +')');
+
+            return false;
+        });
+    };
+
     var updateCellContent = function(theObject) {
         theObject.fadeOut('fast', function() {
             $(this).html(theObject.data('course')).fadeIn('fast');
@@ -211,6 +247,10 @@ var HORARIO = new function() {
     var enhance = function() {
         $('#main td.clickable').each(function(theIndex, theElement) {
             $(theElement).click(handleCellClick);
+        });
+
+        $('#main a.add-professor').each(function(theIndex, theElement) {
+            $(theElement).click(handleNewProfessorClick);
         });
     };
 
